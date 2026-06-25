e07462dd5d753acb70ca5d22cc2d6c7da3add8bc
mcfoto-tsalive <sean@mcfoto.co.nz>
2026-06-25 14:05:11 +1200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   subtitle: "Henshaw Cup - Round 4 Match 8",
   homeTeam: "The Jaffas",
   awayTeam: "Ellesmere Swans",
-  csvUrl: "scores.csv",
+  csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vTLA43qwK-Ve4ugGIzfb4-6RhlzpgoodJtfPHQzAIheXALZ5EhgeiOr8UdcWb9gNuo6Zdx75xLU-n-0/pub?gid=1744440335&single=true&output=csv",
   refreshMs: 30000,
   mobilePage: "mobile.html",
   showQRCode: true
